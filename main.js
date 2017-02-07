4ed92bf3f48de71c16b86f041130a208b4a7865f
nyxlimbs <aheinemann13@gmail.com>
2017-02-07 11:20:04 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var sketchProc= function(processingInstance) {
 with(processingInstance) {
 size(400, 400);
 frameRate(30);
-//Write your code here
+  fill(20, 22, 20);
+text("This is a placeholder for a magical colaboration", 200, 200); //Write your code here
 }};
 var canvas = document.getElementById("myCanvas");
 var processingInstance = new Processing(canvas, sketchProc);
